4341cd0288c3198109b0e4f4489c17d18dc8d232
Zillur2023 <zillurrahmanbd12@gmail.com>
2025-01-13 10:09:24 +0600

    Setup linkup project.Connect with mongodb, handleError,handleNotFound and basic setup

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express, { Application } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import config from "./app/config";
+import router from "./app/routes";
+import globalErrorHandler from "./app/middleware/globalErrorhandler";
+import notFound from "./app/middleware/notFound";
+
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({ origin: [config.client_url as string], credentials: true }));
+
+app.use("/api/v1", router);
+
+app.use(globalErrorHandler);
+
+app.use(notFound);
+
+export default app;
